bf68beee6e60aa728f7e46638ef55e2db3823308
sergiotto <sergiotto@outlook.com>
2023-01-06 20:39:21 +0100

    laoder index

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import Layout from "./components/Layout";
 import NuevoCliente from "./pages/NuevoCliente";
 Index;
 import "./index.css";
-import Index from "./pages/Index";
+import Index, { loader as clientesLoader } from "./pages/Index";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Index /> },
+      { index: true, element: <Index />, loader: clientesLoader },
       { path: "/clientes/nuevo", element: <NuevoCliente /> },
     ],
   },
